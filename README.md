851ac01d0dfebfd62a7cefdca2a265dd6d54a071
Linpeng Lu <lu_lin_peng@qq.com>
2025-08-28 22:27:37 +0800

    add graph for kaprekar diffs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Kaprekar
 Kaprekar number
+
+# Usage
+```shell
+python3 kaprekar.py --digits 3 --outfile 1000.txt
+```
+
+# Result
+![kaprekar_1000](./pics/1000.svg)
--- kaprekar.py
+++ kaprekar.py
@@ -26,15 +26,17 @@ def draw_kaprekar(numbers:dict, l:int, outfile:str=None):
     graph += '}'
     with open(outfile, 'w') as f:
         f.write(graph)
+    print(f'outfile: {outfile}')
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='explore Kaprekar number')
     parser.add_argument("--digits", type=int, default=3, help="number of digits")
+    parser.add_argument("--outfile", type=str, default=None, help="output file of graph")
     args = parser.parse_args()
     l = args.digits
     n = pow(10, l)
     numbers = [i for i in range(n)]
     diffs = kaprekar_diffs(numbers, l)
     print(f'diff ({len(diffs)}): {diffs}')
-    draw_kaprekar(diffs, l)
+    draw_kaprekar(diffs, l, outfile=args.outfile)
